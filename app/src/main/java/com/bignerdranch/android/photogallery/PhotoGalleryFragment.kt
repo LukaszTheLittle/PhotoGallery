4433a2e2abf883fe039ecbe339e1c27dfe772167
LukaszTheLittle <lukis.maly@gmail.com>
2022-06-27 19:41:34 +0200

    Executed the request asynchronously

--- app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.kt
@@ -1,6 +1,7 @@
 package com.bignerdranch.android.photogallery
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,6 +10,8 @@ import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bignerdranch.android.photogallery.api.FlickrApi
 import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import retrofit2.Retrofit
 
 class PhotoGalleryFragment: Fragment() {
@@ -26,6 +29,15 @@ class PhotoGalleryFragment: Fragment() {
         val flickrApi: FlickrApi = retrofit.create(FlickrApi::class.java)
 
         val flickrHomePageRequest: Call<String> = flickrApi.fetchContents()
+
+        flickrHomePageRequest.enqueue(object: Callback<String> {
+            override fun onFailure(call: Call<String>, t: Throwable) {
+                Log.e(TAG, "Failed to fetch photos", t)
+            }
+            override fun onResponse(call: Call<String>, response: Response<String>) {
+            Log.d(TAG, "Response received: ${response.body()}")
+            }
+        })
     }
 
     override fun onCreateView(
@@ -42,6 +54,7 @@ class PhotoGalleryFragment: Fragment() {
     }
 
     companion object {
+        private const val TAG = "PhotoGalleryFragment"
         fun newInstance() = PhotoGalleryFragment()
     }
 }
