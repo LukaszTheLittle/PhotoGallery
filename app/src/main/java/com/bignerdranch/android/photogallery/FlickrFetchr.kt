7349ea21866af6c1187ee1e35bec2240addadfd9
LukaszTheLittle <lukis.maly@gmail.com>
2022-06-27 23:50:44 +0200

    Added `fetchCintents()` to `FlickrFetchr` class

--- app/src/main/java/com/bignerdranch/android/photogallery/FlickrFetchr.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/FlickrFetchr.kt
@@ -1,6 +1,12 @@
 package com.bignerdranch.android.photogallery
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.bignerdranch.android.photogallery.api.FlickrApi
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.scalars.ScalarsConverterFactory
 
@@ -15,6 +21,22 @@ class FlickrFetchr {
             .build()
 
         flickrApi = retrofit.create(FlickrApi::class.java)
+    }
+
+    fun fetchContents(): LiveData<String> {
+        val responseLiveData: MutableLiveData<String> = MutableLiveData()
+        val flickrRequest: Call<String> = flickrApi.fetchContents()
+
+        flickrRequest.enqueue(object: Callback<String> {
+            override fun onFailure(call: Call<String>, t: Throwable) {
+                Log.e(TAG, "Failed to fetch photos", t)
+            }
+            override fun onResponse(call: Call<String>, response: Response<String>) {
+                Log.d(TAG, "Response received")
+                responseLiveData.value = response.body()
+            }
+        })
+        return responseLiveData
     }
 
     companion object {
