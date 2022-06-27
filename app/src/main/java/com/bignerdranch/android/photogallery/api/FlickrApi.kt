7fb4e237a4b8d631f4564635c8c38cdb248aa8f1
LukaszTheLittle <lukis.maly@gmail.com>
2022-06-28 00:18:59 +0200

    Defined request

--- app/src/main/java/com/bignerdranch/android/photogallery/api/FlickrApi.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/api/FlickrApi.kt
@@ -5,6 +5,16 @@ import retrofit2.http.GET
 
 interface FlickrApi {
 
-    @GET("/")
+    @GET(
+        "services/rest/?method=flickr.interestingness.getList" +
+                "&api_key=${API_KEY}" +
+                "&format=json" +
+                "&nojsoncallback=1" +
+                "&extras=url_s"
+    )
     fun fetchContents(): Call<String>
+
+    companion object {
+        private const val API_KEY = "93a8e77d2715f695e9e6f303bb8c739a"
+    }
 }
