2c9355ca226782018ff823aa80c43cb15c4c647c
LukaszTheLittle <lukis.maly@gmail.com>
2022-06-27 20:07:11 +0200

    Cut Retrofit setup

--- app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.kt
@@ -22,13 +22,6 @@ class PhotoGalleryFragment: Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val retrofit: Retrofit = Retrofit.Builder()
-            .baseUrl("https://www.flickr.com")
-            .addConverterFactory(ScalarsConverterFactory.create())
-            .build()
-
-        val flickrApi: FlickrApi = retrofit.create(FlickrApi::class.java)
-
         val flickrHomePageRequest: Call<String> = flickrApi.fetchContents()
 
         flickrHomePageRequest.enqueue(object: Callback<String> {
