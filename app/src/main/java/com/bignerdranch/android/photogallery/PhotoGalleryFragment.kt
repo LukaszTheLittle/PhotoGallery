c898e99d294b216734b5f311d7b0824166386f2a
LukaszTheLittle <lukis.maly@gmail.com>
2022-07-01 02:06:19 +0200

    Added ViewHolder implementation

--- app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -42,6 +43,11 @@ class PhotoGalleryFragment: Fragment() {
             Observer { galleryItems ->
                 Log.d(TAG, "Have gallery items from ViewModel $galleryItems")
             })
+    }
+
+    private class PhotoHolder(itemTextView: TextView): RecyclerView.ViewHolder(itemTextView) {
+
+        val bindTitle: (CharSequence) -> Unit = itemTextView::setText
     }
 
     companion object {
