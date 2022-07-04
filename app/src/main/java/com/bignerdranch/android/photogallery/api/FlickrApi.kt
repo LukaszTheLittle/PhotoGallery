19208fa57e10949086668af3567d5b56756b4a88
LukaszTheLittle <lukis.maly@gmail.com>
2022-07-04 22:37:18 +0200

    Changed https adress to `private const val`

--- app/src/main/java/com/bignerdranch/android/photogallery/api/FlickrApi.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/api/FlickrApi.kt
@@ -7,13 +7,7 @@ import retrofit2.http.Url
 
 interface FlickrApi {
 
-    @GET(
-        "services/rest/?method=flickr.interestingness.getList" +
-                "&api_key=${API_KEY}" +
-                "&format=json" +
-                "&nojsoncallback=1" +
-                "&extras=url_s"
-    )
+    @GET(HTTPS_ADRESS)
     fun fetchPhotos(): Call<FlickrResponse>
 
     @GET
@@ -21,5 +15,10 @@ interface FlickrApi {
 
     companion object {
         private const val API_KEY = "93a8e77d2715f695e9e6f303bb8c739a"
+        private const val HTTPS_ADRESS = "services/rest/?method=flickr.interestingness.getList" +
+                "&api_key=${API_KEY}" +
+                "&format=json" +
+                "&nojsoncallback=1" +
+                "&extras=url_s"
     }
 }
