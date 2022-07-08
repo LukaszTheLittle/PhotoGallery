cf6670b610d61b5986fbf82c3eef2574eaff0be8
LukaszTheLittle <lukis.maly@gmail.com>
2022-07-08 18:40:21 +0200

    Initial thread core

---
+++ app/src/main/java/com/bignerdranch/android/photogallery/ThumbnailDownloader.kt
@@ -0,0 +1,22 @@
+package com.bignerdranch.android.photogallery
+
+import android.os.HandlerThread
+import android.util.Log
+
+class ThumbnailDownloader<in T>: HandlerThread(TAG) {
+
+    private val hasQuit = false
+
+    override fun quit(): Boolean {
+        hasQuit = true
+        return super.quit()
+    }
+
+    fun queueThumbnail(target: T, url: String) {
+        Log.i(TAG, "Got a URL: $url")
+    }
+
+    companion object {
+        private const val TAG = "ThumbnailDownloader"
+    }
+}
