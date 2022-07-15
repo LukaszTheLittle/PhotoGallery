9536c0f6719d1602de647c7a6f01c263cb039116
LukaszTheLittle <lukis.maly@gmail.com>
2022-07-15 18:11:47 +0200

    Changed parameters to const vals

--- app/src/main/java/com/bignerdranch/android/photogallery/api/PhotoInterceptor.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/api/PhotoInterceptor.kt
@@ -11,11 +11,11 @@ class PhotoInterceptor: Interceptor {
         val originalRequest: Request = chain.request()
 
         val newUrl: HttpUrl = originalRequest.url().newBuilder()
-            .addQueryParameter("api_key", API_KEY)
-            .addQueryParameter("format", "json")
-            .addQueryParameter("nojsoncallback", "1")
-            .addQueryParameter("extras", "url_s")
-            .addQueryParameter("safesearch", "1")
+            .addQueryParameter(API_KEY, MY_API_KEY)
+            .addQueryParameter(FORMAT, JSON)
+            .addQueryParameter(NOJSONCALLBACK, VALUE_1)
+            .addQueryParameter(EXTRAS, URL_S)
+            .addQueryParameter(SAFESEARCH, VALUE_1)
             .build()
 
         val newRequest: Request = originalRequest.newBuilder()
@@ -26,6 +26,14 @@ class PhotoInterceptor: Interceptor {
     }
 
     companion object {
-        private const val API_KEY = "93a8e77d2715f695e9e6f303bb8c739a"
+        private const val API_KEY = "api_key"
+        private const val MY_API_KEY = "93a8e77d2715f695e9e6f303bb8c739a"
+        private const val FORMAT = "format"
+        private const val JSON = "json"
+        private const val NOJSONCALLBACK = "nojsoncallback"
+        private const val VALUE_1 = "1"
+        private const val EXTRAS = "extras"
+        private const val URL_S = "url_s"
+        private const val SAFESEARCH = "safesearch"
     }
 }
