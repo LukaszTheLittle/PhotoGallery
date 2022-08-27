531fba11d90418cba715498d84782bf9170d0c45
LukaszTheLittle <lukis.maly@gmail.com>
2022-08-27 22:34:35 +0200

    Checking for new photos

--- app/src/main/java/com/bignerdranch/android/photogallery/PollWorker.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/PollWorker.kt
@@ -1,11 +1,12 @@
 package com.bignerdranch.android.photogallery
 
 import android.content.Context
+import android.util.Log
 import androidx.work.Worker
 import androidx.work.WorkerParameters
 
-class PollWorker(val context: Context, workerParams: WorkerParameters)
-    : Worker(context, workerParams) {
+class PollWorker(val context: Context, workerParameters: WorkerParameters)
+    : Worker(context, workerParameters) {
 
     override fun doWork(): Result {
         val query = QueryPreferences.getStoredQuery(context)
@@ -23,7 +24,22 @@ class PollWorker(val context: Context, workerParams: WorkerParameters)
                 ?.photos
                 ?.galleryItems
         } ?: emptyList()
+
+        if (items.isEmpty()) {
+            return Result.success()
+        }
+
+        val resultId = items.first().id
+        if (resultId == lastResultId) {
+            Log.i(TAG, "Got an old result: $resultId")
+        } else {
+            Log.i(TAG, "Got a new result: $resultId")
+            QueryPreferences.setLastResultId(context, resultId)
+        }
         return Result.success()
     }
 
+    companion object {
+        private const val TAG = "PollWorker"
+    }
 }
