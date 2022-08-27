904a779b5c0bd4157c0ff3958de8510b91771ea4
LukaszTheLittle <lukis.maly@gmail.com>
2022-08-27 18:14:18 +0200

    Saving the latest photo ID

--- app/src/main/java/com/bignerdranch/android/photogallery/QueryPreferences.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/QueryPreferences.kt
@@ -18,5 +18,17 @@ object QueryPreferences {
             }
     }
 
+    fun getLastResultId(context: Context): String {
+        return PreferenceManager.getDefaultSharedPreferences(context)
+            .getString(PREF_LAST_RESULT_ID, "")!!
+    }
+
+    fun setLastResultId(context: Context, lastResultId: String) {
+        PreferenceManager.getDefaultSharedPreferences(context).edit {
+            putString(PREF_LAST_RESULT_ID, lastResultId)
+        }
+    }
+
     private const val PREF_SEARCH_QUERY = "searchQuery"
+    private const val PREF_LAST_RESULT_ID = "lastResultId"
 }
