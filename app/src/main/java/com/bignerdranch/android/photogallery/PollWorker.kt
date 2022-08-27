5d825945a78ced1dde972e3b54935f93e982c481
LukaszTheLittle <lukis.maly@gmail.com>
2022-08-27 22:19:05 +0200

    Fetching recent photos

--- app/src/main/java/com/bignerdranch/android/photogallery/PollWorker.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/PollWorker.kt
@@ -1,7 +1,6 @@
 package com.bignerdranch.android.photogallery
 
 import android.content.Context
-import android.util.Log
 import androidx.work.Worker
 import androidx.work.WorkerParameters
 
@@ -9,11 +8,22 @@ class PollWorker(val context: Context, workerParams: WorkerParameters)
     : Worker(context, workerParams) {
 
     override fun doWork(): Result {
-        Log.i(TAG, "Work request triggered")
+        val query = QueryPreferences.getStoredQuery(context)
+        val lastResultId = QueryPreferences.getLastResultId(context)
+        val items: List<GalleryItem> = if (query.isEmpty()) {
+            FlickrFetchr().fetchPhotosRequest()
+                .execute()
+                .body()
+                ?.photos
+                ?.galleryItems
+        } else {
+            FlickrFetchr().searchPhotosRequest(query)
+                .execute()
+                .body()
+                ?.photos
+                ?.galleryItems
+        } ?: emptyList()
         return Result.success()
     }
 
-    companion object {
-        private const val TAG = "PollWorker"
-    }
 }
